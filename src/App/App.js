ebf0c8f811d4b1705d79787a6fee8d5e42d9fffc
m7anj <45573549+mxnjv@users.noreply.github.com>
2025-03-30 19:54:44 +0100

    File Structure Edition

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function Home() {
+  return (
+    <div className="Home-container">
+
+      <h1>Manjeev Takhar</h1>
+      <p>Portfolio Website - Computer Science Student</p>
+
+    </div>
+  );
+}
+
+export default Home;
