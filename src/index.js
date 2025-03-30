57c18b8ce505a6d389103502003b87dcf127aff0
m7anj <45573549+mxnjv@users.noreply.github.com>
2025-03-30 20:03:32 +0100

    Changes to the file structure & changing of the css.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Home from './App/App.js';
-import './App/App.css';
+import App from './App.js';
+import './App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>
 );
